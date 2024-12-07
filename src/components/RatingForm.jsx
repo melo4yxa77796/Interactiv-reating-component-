1925a7568fd6d32844ee9cd121f8e9e5a5e83e18
melo4yxa77796 <g_julija_777@mail.ru>
2024-12-07 13:14:06 +0100

    modify files

--- src/components/RatingForm.jsx
+++ src/components/RatingForm.jsx
@@ -14,7 +14,8 @@ if(selectedRating){
     };
     return (
         <div className="rating-form">
-            
+          <img src="/images/illustration-thank-you.svg" alt="Thank You" />
+
             <h1>How did we do?</h1>
             <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
             <div className="rating">
